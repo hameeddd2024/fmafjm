74c7293f3a3e7ddeae8a89f08d638445f41c6e2a
hameeddd2024 <ht8563629@gmail.com>
2024-08-25 12:30:18 -0700

    Add files via upload

---
+++ js2/js2.js
@@ -0,0 +1,53 @@
+ 
+//    هذا عنصر اظافة
+ document.getElementById("ooo").addEventListener("click",function(){
+
+
+
+     
+    //  let mar = document.getElementById("textw").value
+     
+     let mohaemd  = ["asfadf","safasfaf","afafasfa"] 
+     document.getElementById("textw").value
+
+     document.getElementById("momo").innerHTML= `${mohaemd}`
+     mohaemd.push()
+     console.log(mohaemd)
+     
+    })
+
+
+
+
+
+
+
+     
+         document.getElementById("pppp").addEventListener("click",function(){
+           var mohaemd
+             mohaemd.pop()
+
+    })
+
+
+//    هذا عنصر الجذف
+
+
+    //  let hamed = hamed.pop()
+     
+     
+ 
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
